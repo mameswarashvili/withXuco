2843984b985c74e2b56e2aa7032009d1fc36bac6
Niko <nikoloz.xucishvili655@gmail.com>
2024-08-03 23:43:46 +0400

    React app

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("close-button").onclick = function() {
-    // Hide the overlay
-    document.getElementById("overlay").style.display = "none";
-};
-
-// Optionally, you can show the overlay on page load
-window.onload = function() {
-    document.getElementById("overlay").style.display = "flex"; // Show overlay on page load
-};
