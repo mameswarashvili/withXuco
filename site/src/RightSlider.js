dcb7a824a883c2c3a7419dc06893f3e2e9e48bdc
NikaXucishvili <nikoloz.xucishvili655@gmail.com>
2024-08-22 00:20:52 +0400

    new file

---
+++ site/src/RightSlider.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './RightSlider.css';
+
+const RightSlider = () => {
+  return (
+    <div className="content-container">
+      <div className="content-box">
+        <h1>Explore Our Top Picks!</h1>
+        <p>Discover a selection of our best products. From stylish fashion to the latest tech, we have something for everyone. Check out our curated collection and find your next favorite item.</p>
+        <button className="explore-button">Explore Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default RightSlider;
